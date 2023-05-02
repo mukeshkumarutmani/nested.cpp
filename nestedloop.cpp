88f461f1278c5597ece5f60c83b9c47bc1044a13
Mukesh Kumar Utmani <109408900+mukeshkumarutmani@users.noreply.github.com>
2023-05-02 12:39:20 +0500

    Update nestedloop.cpp

--- nestedloop.cpp
+++ nestedloop.cpp
@@ -1,10 +1,10 @@
-// # include <iostream>
-// using namespace std;
-// int main()
+ # include <iostream>
+  using namespace std;
+             int main()
 
-// {
-//    int i,j,k;
-//    for(i=1; i<=5; i++)
+ {
+   int i,j,k;
+    for(i=1; i<=5; i++)
 //    {
 //     for(j=5; j>=i; --j)
 
